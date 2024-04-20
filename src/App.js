afc138d739b00b4dea22f2dd982f6af58a474b26
manjunadhb <manjunadhb@gmail.com>
2024-04-20 17:33:33 +0530

    Update App.js
    added second heading.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>This heading is added by Manjunadh from local repository.</h1>
+    <h1>This is the code normally added by team members.</h1>
     </div>
   );
 }
